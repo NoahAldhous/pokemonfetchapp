6291295db20334007326c8ed2e6d13b754a3e1e2
NoahAldhous <naldhous@hotmail.co.uk>
2022-09-23 16:48:58 +0100

    disabled exhaustive dependencies error for movelist component

---
+++ plan.js
@@ -0,0 +1,14 @@
+/*
+    POKEBRAWLZ
+
+    STILL TO DO 
+        onClick of fight button-
+            Random move chosen by CPU 
+            Displays that move-
+            Determines which pokemon goes first (pokemon.speed)
+            Compares the power of both moves 
+            
+
+            
+
+*/
--- src/Components/MoveList/MoveList.js
+++ src/Components/MoveList/MoveList.js
@@ -19,6 +19,7 @@ export default function MoveList({pokemon, setPlayerMove}){
 
     useEffect(() => {
         getMoves()
+      // eslint-disable-next-line react-hooks/exhaustive-deps
       }, [pokemon]);
     
 
