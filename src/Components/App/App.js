201bc44df446ff903d139d64993da866df445e17
NoahAldhous <naldhous@hotmail.co.uk>
2022-11-07 13:19:54 +0000

    player move power and chance to hit now display on hover, player pokemon now displays HP, Speed, and chosen move name, power and accuracy

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,13 +9,14 @@ function App() {
 
   const [playerPokemon, setPlayerPokemon ] = useState(null);
   const [computerPokemon, setComputerPokemon ] = useState(null);
-  const [playerScore] = useState(0);
-  const [computerScore] = useState(0);
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
   const [playerMove, setPlayerMove] = useState('');
   const [computerMove, setComputerMove] = useState('');
   const [isOpen, setIsOpen] = useState(false);
   const [resultsMessage, setResultsMessage] = useState('')
   const [actionReport, setActionReport] = useState('')
+  const [chosenMove, setChosenMove] = useState('')
 
 
   async function getPlayerPokemon(){
@@ -23,6 +24,7 @@ function App() {
     let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${ranNum}`);
     let data = await res.json();
     setPlayerPokemon(data)
+    console.log(playerPokemon)
   }
 
   async function getComputerPokemon(){
@@ -35,29 +37,33 @@ function App() {
   useEffect(() => {
     getPlayerPokemon(); 
     getComputerPokemon(); 
+    // eslint-disable-next-line
   }, []);
 
   function handleReset(){
     setIsOpen(false);
     setPlayerMove('')
+    setChosenMove('')
     getPlayerPokemon(); 
     getComputerPokemon(); 
   }
 
   function handleFight(){
-    if(playerMove && computerMove){
+    if(chosenMove && computerMove){
       setIsOpen(true)
       setActionReport(
-        `${playerPokemon.name.toUpperCase()} used ${playerMove.name.toUpperCase()}, ${computerPokemon.name.toUpperCase()} used ${computerMove.name.toUpperCase()} `
+        `${playerPokemon.name.toUpperCase()} used ${chosenMove.name.toUpperCase()}, ${computerPokemon.name.toUpperCase()} used ${computerMove.name.toUpperCase()} `
       )
-      if(playerMove.power === computerMove.power){
+      if(chosenMove.power === computerMove.power){
         setResultsMessage("It's a draw");
       }
-      else if(playerMove.power < computerMove.power){
+      else if(chosenMove.power < computerMove.power){
         setResultsMessage('CPU Wins!');
+        setComputerScore(computerScore + 1)
       }
-      else if(playerMove.power > computerMove.power){
+      else if(chosenMove.power > computerMove.power){
         setResultsMessage('Player Wins!');
+        setPlayerScore(playerScore + 1)
       }
       else{
         setResultsMessage('something went wrong')
@@ -75,15 +81,17 @@ function App() {
         <h1 className= 'heading'> Pokebrawlz</h1>
         <Modal open = {isOpen} onClose = {handleReset} results = {resultsMessage} actionReport = {actionReport}></Modal>
         <section className = "pokemon-container">
-          <section className = 'score-container'>
-            <div className = 'move-display'> MOVE CHOSEN : <br/> {playerMove.name}</div>
-            <div className = 'move-display'> POWER : <br/> {playerMove.power}</div>
-            <div className = 'score-display'> PLAYER:{playerScore} </div>
+          <section className = 'move-info-container'>
+            <div className = 'hp-display'> {playerPokemon.stats[0].stat.name.toUpperCase()}: {playerPokemon.stats[0].base_stat}</div>
+            <div className = 'speed-display'> {playerPokemon.stats[5].stat.name.toUpperCase()}: {playerPokemon.stats[5].base_stat}</div>
+            <div className = 'move-name-display'> MOVE CHOSEN: {chosenMove.name}</div>
+            <div className = 'move-accuracy-display'> ACCURACY: {chosenMove.accuracy}</div>
+            <div className = 'move-power-display'> POWER: {chosenMove.power}</div>
           </section>
           <section className = "pokemon-info-container">
             <h2 className = 'pokemon-name'> {playerPokemon.name.toUpperCase()} </h2>
             <Image pokemon = {playerPokemon}/>
-            <PlayerMoveList pokemon = {playerPokemon} setPlayerMove = {setPlayerMove}/>
+            <PlayerMoveList pokemon = {playerPokemon} playerMove = {playerMove} setPlayerMove = {setPlayerMove} chosenMove = {chosenMove} setChosenMove = {setChosenMove}/>
           </section>
           <div className = 'versus-text'> VS </div>
           <section className = "pokemon-info-container">
@@ -91,10 +99,11 @@ function App() {
             <Image pokemon = {computerPokemon}/>
             <ComputerMoveList pokemon = {computerPokemon} setComputerMove = {setComputerMove}/>
           </section>
-          <section className = 'score-container'>
-            <div className = 'score-display'> CPU:{computerScore} </div>
-            <div className = 'move-display'> MOVE CHOSEN : <br/> {computerMove.name}</div>
-            <div className = 'move-display'> POWER : <br/> {computerMove.power}</div>
+          <section className = 'move-info-container'>
+            <div className = 'score-display'> CPU: {computerScore} </div>
+            <div className = 'move-display'> MOVE CHOSEN: {computerMove.name}</div>
+            <div className = 'move-display'> POWER: {computerMove.power}</div>
+            <div className = 'speed-display'> {computerPokemon.stats[5].stat.name.toUpperCase()}: {computerPokemon.stats[5].base_stat}</div>
           </section>
         </section>
         <section className = 'pokemon-button-container'>
--- src/Components/MoveList/PlayerMoveList.js
+++ src/Components/MoveList/PlayerMoveList.js
@@ -2,7 +2,7 @@ import './PlayerMoveList.css';
 import {useState, useEffect} from 'react'
 
 
-export default function MoveList({pokemon, setPlayerMove}){
+export default function MoveList({pokemon, playerMove, setPlayerMove, chosenMove, setChosenMove}){
     const [moves, setMoves] = useState([]);
     const [activeButton, setActiveButton] = useState('');
     
@@ -33,14 +33,22 @@ export default function MoveList({pokemon, setPlayerMove}){
     async function getMoveData(move){
         let res = await fetch(move.url);
         let data = await res.json();
+        console.log(data)
         if(data.power === null){
             data.power = 0;
+        }
+        if(data.accuracy === null){
+            data.accuracy = 100;
         }
         console.log(`name:${data.name} power:${data.power}`)
         setPlayerMove({
             name: data.name,
-            power: data.power
+            power: data.power,
+            accuracy: data.accuracy
         });
+    }
+    function handleButtonClick(move, playerMove){
+        setChosenMove(playerMove)
         setActiveButton(move.name);
         console.log(activeButton);
         document.querySelectorAll(".pokemon-move").forEach((item) => {
@@ -52,12 +60,18 @@ export default function MoveList({pokemon, setPlayerMove}){
         document.querySelector(`.${move.name}`).classList.toggle('active-button');
     }
 
-
-
+    function resetPlayerMove(){
+        setPlayerMove('')
+    }
 
     return(
         moves.map((move)=>{
-            return <h3 onClick = {function(){getMoveData(move)}} className = {['pokemon-move', `${move.name}`].join(' ')}> {move.name.toUpperCase()}</h3>
+            
+            return <h3 onMouseEnter = {function(){getMoveData(move)}} onMouseLeave = {resetPlayerMove} onClick = {function(){handleButtonClick(move, playerMove)}} className = {['pokemon-move', `${move.name}`].join(' ')}>
+            <div className='move-info'>CHANCE TO HIT: {playerMove.accuracy}% POWER: {playerMove.power}</div>
+             {move.name.toUpperCase()}
+             </h3>
+
         }
         )
     )
