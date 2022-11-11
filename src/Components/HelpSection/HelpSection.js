cccfc7900effc5e0bdb165770471d38e367f7ed7
NoahAldhous <naldhous@hotmail.co.uk>
2022-11-11 01:06:05 +0000

    added extra help section text, added bold text to highlight keywods in the help text

--- src/Components/HelpSection/HelpSection.js
+++ src/Components/HelpSection/HelpSection.js
@@ -26,28 +26,51 @@ export default function HelpSection({wantsHelp, setWantsHelp}){
                     THE POKEMON
                 </h3>
                 <p className = 'help-section-text'>
-                    Each pokemon has different stats, shown next to their picture. Higher stats give a pokemon an edge in battle, but don't guarantee victory!
+                    Each pokemon has different stats, shown next to their picture. Higher stats give a pokemon an edge in battle, but they don't guarantee victory!
                     <br/>
                     <br/>
-                    HP: The HIT POINTS of a pokemon. Reduced by opponent's ATTACKS.
+                    <b>HP</b>: The <b>HIT POINTS</b> of a pokemon. Reduced by opponent's <b>ATTACKS</b>. Once a pokemon's <b>HIT POINTS</b> reach 0, they die. 
                     <br/>
                     <br/>
-                    SPEED: How fast a pokemon is. The Pokemon with the higher SPEED is more likely to act first.
+                    <b>SPEED</b>: How fast a pokemon is. The Pokemon with the higher <b>SPEED</b> is more likely to act first.
                     <br/>
                     <br/>
-                    CRITICAL CHANCE: When a pokemon uses an ATTACK, there is a chance it is a CRITICAL HIT. It DOUBLES the DAMAGE dealt to the opponent's HIT POINTS. This chance can be increased by using a FOCUS MOVE.
+                    <b>CRITICAL CHANCE</b>: When a pokemon uses an <b>ATTACK</b>, there is a chance it is a <b>CRITICAL HIT</b>. It <b>DOUBLES</b> the <b>DAMAGE</b> dealt to the opponent's <b>HIT POINTS</b>.
                 </p>
                 <h3 className = 'help-section-subheader'>
                     THE MOVES
                 </h3>
                 <p className = 'help-section-text'>
-                        
+                    Every move belongs to one of two categories: it is either an <b>ATTACK</b> or a <b>SPECIAL MOVE</b>. 
+                    <br/>
+                    <br/>
+                    <b>ATTACKS</b> deal damage directly to a pokemon. If they hit, they reduce the opponent's <b>HIT POINTS</b>.
+                    <br/>
+                    <br/>
+                    <b>SPECIAL MOVES</b> don't deal any damage, but affect the <b>STATS</b> of a pokemon. Some weaken the opponent's <b>STATS</b>, others boost your own. 
+                    <br/>
+                    <br/>
+                    <b>ACCURACY</b>: This is the percentage chance that your chosen <b>MOVE</b> will succeed. Most <b>SPECIAL MOVES</b> have an accuracy of 100, whereas <b>ATTACKS</b> are more likely to miss.
+                    <br/>
+                    <br/>
+                    <b>POWER/EFFECT</b>: If a <b>MOVE</b> is an <b>ATTACK</b>, it will have a <b>POWER</b> stat. This is how many <b>HIT POINTS</b> it will take from the opponent if it hits. If the <b>ATTACK</b> is a <b>CRITICAL HIT</b>, the <b>POWER</b> is doubled (for one round only).
+                    <br/>
+                    If a MOVE is a SPECIAL MOVE, it will have an EFFECT. How each EFFECT works is detailed below. SPECIAL MOVES always have a POWER of 0, and are not affected by CRITICAL HITS.
                 </p>
                 <h3 className = 'help-section-subheader'>
                     SPECIAL EFFECTS
                 </h3>
                 <p className = 'help-section-text'>
-                        
+                   There are three kinds of SPECIAL MOVES: DEFEND, DODGE and FOCUS.
+                   <br/>
+                   <br/> 
+                   DEFEND: If successful, a DEFEND move reduces your opponent's ATTACK POWER by 50% for the current round.
+                   <br/>
+                   <br/> 
+                   DODGE: If successful, a DODGE move reduces your opponent's ACCURACY by 50% for the current round.
+                   <br/>
+                   <br/> 
+                   FOCUS: If successful, a FOCUS move will increase your Pokemon's critical chance by 20% until the end of your next turn.
                 </p>
                 <h3 className = 'help-section-subheader'>
                     THE BATTLE REPORT
